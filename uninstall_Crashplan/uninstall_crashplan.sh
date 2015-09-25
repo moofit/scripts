1c9404f302e180ce89f7dedb76ed81cd4aa9c7a5
David Acland <davida@amsys.co.uk>
2015-09-25 20:10:54 +0200

    Added uninstall Crashplan script

---
+++ uninstall_Crashplan/uninstall_crashplan.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo | osascript <<EOF
+tell application "Crashplan.app"
+  quit
+end tell
+EOF
+
+sleep 5
+
+launchctl unload /Library/LaunchDaemons/com.crashplan.engine.plist
+
+chflags noschg
+
+rm -R /Applications/CrashPlan.app
+rm -R /Library/Logs/CrashPlan
+rm -R /Users/$USER/Library/Logs/CrashPlan
+rm -R /Library/Application\ Support/CrashPlan
+rm -R /Library/Caches/CrashPlan
+rm -R /Library/LaunchDaemons/com.crashplan.engine.plist
+
+exit 0
