084ba58e6b0f5794e8c57054cdbf0bf5a3f6b1ff
David Acland <davida@amsys.co.uk>
2015-11-09 15:19:18 +0000

    Updated first boot

--- first_boot/10.10_firstBoot.sh
+++ first_boot/10.10_firstBoot.sh
@@ -12,7 +12,6 @@
 ## variables ##
 ###############
 PLBUDDY=/usr/libexec/PlistBuddy
-HOMEPAGE="www.amsys.co.uk"
 KEYBOARDNAME="British"
 KEYBOARDCODE="2"
 LANG="en"
@@ -244,7 +243,7 @@ done
 # Turn off restore windows
 for USER_TEMPLATE in "/System/Library/User Template"/*
 	do
-	/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/ .GlobalPreferences" NSQuitAlwaysKeepsWindows -boolean FALSE
+	/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/.GlobalPreferences NSQuitAlwaysKeepsWindows -boolean FALSE
 done
 
 for USER_HOME in /Users/*
@@ -261,7 +260,7 @@ for USER_HOME in /Users/*
 			if [ -d "${USER_HOME}"/Library/Preferences ]
 			then
 			killall -u $USER_UID cfprefsd
-			/usr/bin/defaults write "${USER_HOME}"/Library/Preferences/ .GlobalPreferences" NSQuitAlwaysKeepsWindows -boolean FALSE
+			/usr/bin/defaults write "${USER_HOME}"/Library/Preferences/.GlobalPreferences NSQuitAlwaysKeepsWindows -boolean FALSE
 			fi
 		fi
 done
@@ -290,37 +289,5 @@ for USER_HOME in /Users/*
 			fi
 		fi
 done
-
-# Set the Users Homepage
-for USER_TEMPLATE in "/System/Library/User Template"/*
-	do
-	/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.Safari.plist HomePage -string "$HOMEPAGE"
-    /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.Safari.plist NewTabBehavior -integer 0
-    /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/com.apple.Safari.plist NewWindowBehavior -integer 0
-done
-
-for USER_HOME in /Users/*
-	do
-		USER_UID=`basename "${USER_HOME}"`
-		if [ ! "${USER_UID}" = "Shared" ] 
-		then 
-			if [ ! -d "${USER_HOME}"/Library/Preferences ]
-			then
-			mkdir -p "${USER_HOME}"/Library/Preferences
-			chown "${USER_UID}" "${USER_HOME}"/Library
-			chown "${USER_UID}" "${USER_HOME}"/Library/Preferences
-			fi
-			if [ -d "${USER_HOME}"/Library/Preferences ]
-			then
-				echo "Working on home folder preference file: ${USER_HOME}/Library/Preferences/com.apple.Safari.plist"
-				mv "${USER_HOME}"/Library/Preferences/com.apple.Safari.plist "${USER_HOME}"/Library/Preferences/com.apple.Safari.plist_bak
-				/usr/bin/defaults write "${USER_HOME}"/Library/Preferences/com.apple.Safari.plist HomePage -string "$HOMEPAGE"
-    			/usr/bin/defaults write "${USER_HOME}"/Library/Preferences/com.apple.Safari.plist NewTabBehavior -integer 0
-    			/usr/bin/defaults write "${USER_HOME}"/Library/Preferences/com.apple.Safari.plist NewWindowBehavior -integer 0
-    			chown $USER_UID "${USER_HOME}"/Library/Preferences/com.apple.Safari.plist
-			fi
-		fi
-done
-killall cfprefsd
 
 exit 0
