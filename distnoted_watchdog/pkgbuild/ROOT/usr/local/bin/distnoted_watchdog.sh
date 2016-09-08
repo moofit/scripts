64131bdeb7158bc2c0e16c6acc5d47d5aa8f3478
Stephen Warneford-Bygrave <sbygrave@mac.com>
2016-09-08 12:16:43 +0100

    Added readme with pkgbuild instructions

---
+++ distnoted_watchdog/pkgbuild/ROOT/usr/local/bin/distnoted_watchdog.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PATH=/bin:/usr/bin
+export PATH
+logger "Checking distnoted"
+
+# check for runaway distnoted, kill if necessary
+ps -reo '%cpu,uid,pid,command' |
+    awk -v UID=$UID '
+    /distnoted agent$/ && $1 > 100.0 && $2 == UID {
+        system("kill -9 " $3)
+    }
+    '
---
+++ distnoted_watchdog/readme.md
@@ -0,0 +1,3 @@
+To create an installer, navigate to the "pkgbuild" directory in Terminal and run the following command:
+
+pkgbuild --identifier uk.co.amsys.distnoted_watchdog --root ./ROOT/ --scripts ./scripts ./distnoted_watchdog-1.0.pkg
