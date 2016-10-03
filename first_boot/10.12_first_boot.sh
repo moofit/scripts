f754db20393bfed4de04ed9a66936d95595d4e50
Stephen Bygrave <wegotoeleven@users.noreply.github.com>
2016-10-03 17:08:38 +0100

    Removed incorrect "fi" at line 165

--- first_boot/10.12_first_boot.sh
+++ first_boot/10.12_first_boot.sh
@@ -148,21 +148,20 @@ for USER_TEMPLATE in "/System/Library/User Template"/*
     
 # Disable diagnostics pop-up on 10.10 and above
  
-  if [ ! -d "${CRASHREPORTER_SUPPORT}" ]; then
-    mkdir "${CRASHREPORTER_SUPPORT}"
-    chmod 775 "${CRASHREPORTER_SUPPORT}"
-    chown root:admin "${CRASHREPORTER_SUPPORT}"
-  fi
- 
-  for key in AutoSubmit AutoSubmitVersion ThirdPartyDataSubmit ThirdPartyDataSubmitVersion; do
-    $PLBUDDY -c "Delete :$key" "${CRASHREPORTER_DIAG_PLIST}" 2> /dev/null
-  done
- 
-  $PLBUDDY -c "Add :AutoSubmit bool ${SUBMIT_TO_APPLE}" "${CRASHREPORTER_DIAG_PLIST}"
-  $PLBUDDY -c "Add :AutoSubmitVersion integer 4" "${CRASHREPORTER_DIAG_PLIST}"
-  $PLBUDDY -c "Add :ThirdPartyDataSubmit bool ${SUBMIT_TO_APP_DEVELOPERS}" "${CRASHREPORTER_DIAG_PLIST}"
-  $PLBUDDY -c "Add :ThirdPartyDataSubmitVersion integer 4" "${CRASHREPORTER_DIAG_PLIST}"
+if [ ! -d "${CRASHREPORTER_SUPPORT}" ]; then
+  mkdir "${CRASHREPORTER_SUPPORT}"
+  chmod 775 "${CRASHREPORTER_SUPPORT}"
+  chown root:admin "${CRASHREPORTER_SUPPORT}"
 fi
+
+for key in AutoSubmit AutoSubmitVersion ThirdPartyDataSubmit ThirdPartyDataSubmitVersion; do
+  $PLBUDDY -c "Delete :$key" "${CRASHREPORTER_DIAG_PLIST}" 2> /dev/null
+done
+
+$PLBUDDY -c "Add :AutoSubmit bool ${SUBMIT_TO_APPLE}" "${CRASHREPORTER_DIAG_PLIST}"
+$PLBUDDY -c "Add :AutoSubmitVersion integer 4" "${CRASHREPORTER_DIAG_PLIST}"
+$PLBUDDY -c "Add :ThirdPartyDataSubmit bool ${SUBMIT_TO_APP_DEVELOPERS}" "${CRASHREPORTER_DIAG_PLIST}"
+$PLBUDDY -c "Add :ThirdPartyDataSubmitVersion integer 4" "${CRASHREPORTER_DIAG_PLIST}"
 
 # Set the time zone to London
 /usr/sbin/systemsetup -settimezone "Europe/London"
