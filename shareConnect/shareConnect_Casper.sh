e1875b99fcc9cb5eadc706e1607472142d59be77
David Acland <davida@amsys.co.uk>
2016-11-16 11:06:40 +0000

    Update shareConnect_Casper.sh
    Removed the group check as Jamf Pro can do this natively via LDAP lookups

--- shareConnect/shareConnect_Casper.sh
+++ shareConnect/shareConnect_Casper.sh
@@ -17,14 +17,6 @@ serverName="$5"	# This is the address of the server, e.g. my.fileserver.com
     echo "Server: $5"
 shareName="$6"	# This is the name of the share to mount
     echo "Sharename: $6"
-group="$7"		# This is the name of the group the user needs to be a member of to mount the share
-    echo "Group: $7"
-
-# Check that the user is in the necessary group
-	groupCheck=`dseditgroup -o checkmember -m $username "$group" | grep -c "yes"`
-		if [ "${groupCheck}" -ne 1 ]; then
-		exit 1
-		fi
 		
 # Mount the drive
 	mount_script=`/usr/bin/osascript > /dev/null << EOT
