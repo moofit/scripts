7c1f27ccc99b59089d11b950214365169cc15544
Darren Wallace <Daz-wallace@users.noreply.github.com>
2018-01-22 10:47:55 +0000

    New commit and making things pretty

--- macOS/Software Updates/EA - List_Configuration_Data_Updates.sh
+++ macOS/Software Updates/EA - List_Configuration_Data_Updates.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 #########################################################################################
-# Author:   Darren Wallace - Moof IT
-# Name:     Install_Configuration_Data_Updates.sh
+# Author:   Darren Wallace - Amsys
+# Name:     EA - List_Configuration_Data_Updates.sh
 #
 # Purpose:  This script uses the undocumented '--include-config-data' flag for 
-#			softwareupdate to list and run these updates.
+#			softwareupdate to list these updates ready for an EA.
 #			This includes data for Apple's GateKeeper, X Protect and Malware 
 #			Removal Tool
 #			*NOTE*: This only works on 10.12.x or newer.
 #			*NOTE 2: This has only been tested on 10.12.6 (16G29) and 10.11.x
 # Usage:    CLI
 #
-# Version 2018.01.22 - DW - Typo fix, minor pruning and switch to Moof IT!
-# 		  2017.09.11 - DW - Initial Creation
+# Version 2018.01.22 - DW - Initial Creation
 #
 #########################################################################################
 #
@@ -26,14 +25,14 @@
 #               * Redistributions in binary form must reproduce the above copyright
 #                 notice, this list of conditions and the following disclaimer in the
 #                 documentation and/or other materials provided with the distribution.
-#               * Neither Moof IT Ltd nor the
+#               * Neither Amsys Ltd nor the
 #                 names of its contributors may be used to endorse or promote products
 #                 derived from this software without specific prior written permission.
 #
-#       THIS SOFTWARE IS PROVIDED BY Moof IT LTD "AS IS" AND ANY
+#       THIS SOFTWARE IS PROVIDED BY Amsys LTD "AS IS" AND ANY
 #       EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 #       WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-#       DISCLAIMED. IN NO EVENT SHALL Moof IT LTD BE LIABLE FOR ANY
+#       DISCLAIMED. IN NO EVENT SHALL Amsys LTD BE LIABLE FOR ANY
 #       DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 #       (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 #       LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -45,7 +44,7 @@
 #
 # SUPPORT FOR THIS PROGRAM
 #
-#       This program is distributed "as is" by Moof IT LTD.
+#       This program is distributed "as is" by Amsys LTD.
 #       For more information or support, please utilise the following resources:
 #
 #               http://www.moof-it.co.uk
@@ -55,30 +54,22 @@
 ##################################### Set variables #####################################
 
 # Name of the script
-scriptName="Install_Configuration_Data_Updates"
-
-# Location of the LogFile to save output to
-logFile="/Library/Logs/${scriptName}.log"
+scriptName="EA - List_Configuration_Data_Updates"
 
 ################################## Declare functions ####################################
-
-# Function to write input to the terminal and a logfile
-writeLog ()
-{
-	/bin/echo "$(date) - ${1}"
-	/bin/echo "$(date) - ${1}" >> "${logFile}"
-}
 
 # Function to check the OS is 10.12.x or newer
 checkOS ()
 {
-	writeLog "Checking OS Version..."
+	/bin/echo "Checking OS Version..."
 	osvers=$(/usr/bin/sw_vers -productVersion | /usr/bin/awk -F. '{print $2}')
 	if [[ ${osvers} -ge 12 ]]; 
 	then
-		writeLog "OS Version is 10.12 or newer, proceeding..."
+		/bin/echo "OS Version is 10.12 or newer, proceeding..."
 	else
-		writeLog "OS Version is 10.11 or older and this script will not work."
+		/bin/echo "OS Version is 10.11 or older and this script will not work."
+		result="N/A"
+		/bin/echo "<result>${result}</result>"
 		exit 0
 	fi
 }
@@ -86,33 +77,19 @@ checkOS ()
 # Function to check and list all avalible config data updates
 checkForConfigData ()
 {
-	writeLog "Checking for Config Data updates"
+	/bin/echo "Checking for Config Data updates"
 	updatesFound=$(/usr/sbin/softwareupdate -l --include-config-data | /usr/bin/awk '/\*/ && /ConfigData/' | /usr/bin/sed 's/* //')
 	if [ -z "${updatesFound}" ] || [[ "${updatesFound}" == "" ]];
 	then
-		writeLog "No Config Data updates available."
+		/bin/echo "No Config Data updates available."
+		result="None"
+		/bin/echo "<result>${result}</result>"
 		exit 0
 	else
-		writeLog "Config Data updates found:"
-		writeLog "${updatesFound}"
-	fi
-}
-
-# Function to install all avalible config data updates
-installConfigData ()
-{
-	writeLog "Installing available Config Data updates..."
-	updatesToInstall="${updatesFound} | tr -d '\n'"
-	/usr/sbin/softwareupdate -i ${updatesFound} --include-config-data
-	exitCode="${?}"
-	if [[ "${exitCode}" != 0 ]];
-	then
-		writeLog "ERROR: Error installing updates"
-		writeLog "softwareupdate command error code ${?}"
-		exit "${?}"
-	else
-		writeLog "Updates installed!"
-		exit 0
+		/bin/echo "Config Data updates found:"
+		/bin/echo "${updatesFound}"
+		result="${updatesFound}"
+		/bin/echo "<result>${result}</result>"
 	fi
 }
 
@@ -122,8 +99,6 @@ installConfigData ()
 checkOS
 # Run the undocumented flag for softwareupdate and parse the output
 checkForConfigData
-# Same as above, except this time we'll install using it
-installConfigData
 
-writeLog "ERROR: Unexpected script exit"
+/bin/echo "ERROR: Unexpected script exit"
 exit 2
