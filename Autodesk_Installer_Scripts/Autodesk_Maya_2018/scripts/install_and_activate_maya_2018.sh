16c5ebbce7aa8e20c03256aaf350a783d7c2365e
David Acland <davida@amsys.co.uk>
2018-02-19 14:59:32 +0000

    Update install_and_activate_maya_2018.sh

--- Autodesk_Installer_Scripts/Autodesk_Maya_2018/scripts/install_and_activate_maya_2018.sh
+++ Autodesk_Installer_Scripts/Autodesk_Maya_2018/scripts/install_and_activate_maya_2018.sh
@@ -2,7 +2,7 @@
 
 # Author:   David Acland - Moof IT
 # Name:     postinstall (Maya 2018)
-# Full blog article relating to this script can be found here: https://www.moof-it.co.uk/technical/deploying-autodesk-maya-2018-and-suppressing-first-run-screens
+# Full blog article relating to this script can be found here: https://www.moof-it.co.uk/technical/deploying-autodesk-maya-2018-with-jamf-pro
 #
 # Purpose:  Maya 2018 Post-Install
 # Installs and activates Autodesk Maya 2018 with a standalone license key
